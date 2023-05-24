7fe668542cb7e535db4d3dfae818be8c7d09442e
Anderson <andersonmwp@gmail.com>
2023-05-23 23:12:56 -0300

    finalizado o projeto ranek

--- src/Components/Produto.jsx
+++ src/Components/Produto.jsx
@@ -26,7 +26,7 @@ const Produto = () => {
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <div className="loading"></div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
@@ -35,9 +35,13 @@ const Produto = () => {
         title={`Ranek | ${produto.nome}`}
         description={`Esse é um produto: ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
+
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R$ {produto.preco}</span>
