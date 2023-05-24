319751bcae42a600832880b0f0988515adc444d0
Anderson <andersonmwp@gmail.com>
2023-05-23 22:29:00 -0300

    desenvolvimento da página produtos

--- src/Components/Produtos.jsx
+++ src/Components/Produtos.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import styles from './Produtos.module.css';
 import Head from './Head';
+import { Link } from 'react-router-dom';
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  }, []);
+
+  if (produtos === null) return null;
   return (
-    <div className={styles.produtos}>
-      <Head title="Ranek | Produtos" description="Página de produtos" />
-      <h1>Produtos</h1>
-    </div>
+    <section className={`${styles.produtos} animeLeft`}>
+      <Head title="Ranek" description="Descrição do site Ranek" />
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <h1 className={styles.nome}>{produto.nome}</h1>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+        </Link>
+      ))}
+    </section>
   );
 };
 
